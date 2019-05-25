2ac762a4ac1c006cdc22fadda2e5505a9ad7d751
Jason <m.jason.liu@outlook.com>
2019-05-25 20:36:33 +0800

    ..

--- src/components/Danmaku.vue
+++ src/components/Danmaku.vue
@@ -1,81 +1,79 @@
 <template>
-  <section
-    class="danmaku-wrapper"
-    :style="{
+    <section
+        class="--danmaku-wrapper"
+        :style="{
       color: color,
       'animation-play-state': this.$props.ongoing? 'running': 'paused',
       animation: animation
     }"
-
-    @animationcancel="onAnimationEnd"
-    @animationend="onAnimationEnd"
-  >
-    {{data.content}}
-  </section>
+        @animationcancel="onAnimationEnd"
+        @animationend="onAnimationEnd"
+    >{{data.content}}</section>
 </template>
 
 <script lang="ts">
-  import {Component, Vue} from 'vue-property-decorator'
-  import {BilibiliDanmaku, DanmakuType} from '@/lib/types/danmaku'
+import { Component, Vue } from "vue-property-decorator";
+import { BilibiliDanmaku, DanmakuType } from "@/lib/types/danmaku";
 
-  @Component({
+@Component({
     props: {
-      data: BilibiliDanmaku,
-      ongoing: Boolean
-    },
-  })
-  export default class Danmaku extends Vue {
+        data: BilibiliDanmaku,
+        ongoing: Boolean
+    }
+})
+export default class Danmaku extends Vue {
     color: string = "#FFFFFF";
     animation: string = "";
 
-    $style
+    $style;
 
-    mounted(){
-      let data = this.$props.data as BilibiliDanmaku
-      this.color = `#${(+(data).fontColor).toString(16)}`
-      if(data.getDanmakuType() === DanmakuType.RTL){
-        this.animation = `${this.$style['move-left']} linear 10s`
-      }
-      else if(data.getDanmakuType() === DanmakuType.TOP || data.getDanmakuType() === DanmakuType.BOTTOM){
-        this.animation = `${this.$style['to-disappear']} linear 5s`
-      }
-    }
-
-    onAnimationEnd(){
-      // console.log('endddd', this.$props.data.rowID)
-      this.$emit('onAnimationEnd',
-        {
-          id: this.$props.data.rowID,
-          mode: this.$props.data.getDanmakuType()
+    mounted() {
+        let data = this.$props.data as BilibiliDanmaku;
+        this.color = `#${(+data.fontColor).toString(16)}`;
+        if (data.getDanmakuType() === DanmakuType.RTL) {
+            this.animation = `${this.$style["move-left"]} linear 10s`;
+        } else if (
+            data.getDanmakuType() === DanmakuType.TOP ||
+            data.getDanmakuType() === DanmakuType.BOTTOM
+        ) {
+            this.animation = `${this.$style["to-disappear"]} linear 5s`;
         }
-      )
     }
-  }
+
+    onAnimationEnd() {
+        // console.log('endddd', this.$props.data.rowID)
+        this.$emit("onAnimationEnd", {
+            id: this.$props.data.rowID,
+            mode: this.$props.data.getDanmakuType()
+        });
+    }
+}
 </script>
 
 <style module>
-  @keyframes move-left {
+@keyframes move-left {
     from {
-      transform: translateX(100vw);
+        transform: translateX(100vw);
     }
-    to{
-      transform: translateX(-150vw);
+    to {
+        transform: translateX(-150vw);
     }
-  }
+}
 
-  @keyframes to-disappear {
-    to{
-      display: none;
+@keyframes to-disappear {
+    to {
+        display: none;
     }
-  }
+}
 </style>
 
 <style scoped>
-  .danmaku-wrapper{
+.--danmaku-wrapper {
     font-weight: bold;
     text-shadow: rgb(0, 0, 0) 1px 0px 1px, rgb(0, 0, 0) 0px 1px 1px,
-    rgb(0, 0, 0) 0px -1px 1px, rgb(0, 0, 0) -1px 0px 1px;
+        rgb(0, 0, 0) 0px -1px 1px, rgb(0, 0, 0) -1px 0px 1px;
     font-size: 1.2rem;
     line-height: 1.2rem;
-  }
+    overflow: unset;
+}
 </style>
--- src/components/VideoWrapper.vue
+++ src/components/VideoWrapper.vue
@@ -1,95 +1,83 @@
 <template>
-  <section class="video-wrapper" ref="videoWrapper">
-    <GlobalEvents
-      @mousemove="onMousemove"
-    />
-    <section
-      class="danmaku-wrapper"
-      :style="{width: wrapperWidth}"
-    >
-      <video
-        :src="videoURL"
-        ref="video"
-        @timeupdate="timeChange"
-        @loadedmetadata="loadedmetadata"
-        @durationchange="durationChanged"
-        @play="onPlay"
-        @playing="onPlay"
-        @waiting="onWaiting"
-        @pause="onPause"
-      >
-
-      </video>
-      <section
-        class="danmaku-column"
-        v-for="(dColumn, i) in  normalDanmakuPool"
-        :key="i"
-      >
-        <Danmaku
-          v-for="(d) in dColumn"
-          :key="d.rowID"
-          :data="d"
-          :ongoing="ongoing"
-          @onAnimationEnd="onDanmakuShouldRemove"
+    <section class="video-wrapper" ref="videoWrapper">
+        <GlobalEvents @mousemove="onMousemove"/>
+        <section class="danmaku-wrapper" :style="{width: wrapperWidth}">
+            <video
+                :src="videoURL"
+                ref="video"
+                @timeupdate="timeChange"
+                @loadedmetadata="loadedmetadata"
+                @durationchange="durationChanged"
+                @play="onPlay"
+                @playing="onPlay"
+                @waiting="onWaiting"
+                @pause="onPause"
+            ></video>
+            <section class="danmaku-column" v-for="(dColumn, i) in  normalDanmakuPool" :key="i">
+                <Danmaku
+                    v-for="(d) in dColumn"
+                    :key="d.rowID"
+                    :data="d"
+                    :ongoing="ongoing"
+                    @onAnimationEnd="onDanmakuShouldRemove"
+                />
+            </section>
+            <section class="danmaku-column">
+                <Danmaku
+                    v-for="(item) in topDanmakuPool"
+                    :data="item"
+                    :key="item.rowID"
+                    :ongoing="ongoing"
+                    @onAnimationEnd="onDanmakuShouldRemove"
+                />
+            </section>
+            <section class="danmaku-column-reversed">
+                <Danmaku
+                    v-for="(item) in bottomDanmakuPool"
+                    :data="item"
+                    :key="item.rowID"
+                    :ongoing="ongoing"
+                    @onAnimationEnd="onDanmakuShouldRemove"
+                />
+            </section>
+        </section>
+        <VideoControlbar
+            :currentTime="currentTime"
+            :loaded="loaded"
+            :duration="totalTime"
+            :playState="ongoing"
+            :isFullscreen="isFullscreen"
+            :style="{width: controlbarWidth}"
+            v-show="showControlbar"
+            @btnPlayClicked="btnPlayClicked"
+            @timeChanged="dragged"
+            @volumeChanged="volumeChanged"
+            @reqFullScreen="reqFullScreen"
+            @danmakuInputValueChanged="danmakuInputValueChanged"
         />
-      </section>
-      <section class="danmaku-column">
-        <Danmaku
-          v-for="(item) in topDanmakuPool"
-          :data="item"
-          :key="item.rowID"
-          :ongoing="ongoing"
-          @onAnimationEnd="onDanmakuShouldRemove"
-        />
-      </section>
-      <section class="danmaku-column-reversed">
-        <Danmaku
-          v-for="(item) in bottomDanmakuPool"
-          :data="item"
-          :key="item.rowID"
-          :ongoing="ongoing"
-          @onAnimationEnd="onDanmakuShouldRemove"
-        />
-      </section>
     </section>
-    <VideoControlbar
-      :currentTime="currentTime"
-      :loaded="loaded"
-      :duration="totalTime"
-      :playState="ongoing"
-      :isFullscreen="isFullscreen"
-      :style="{width: controlbarWidth}"
-      v-show="showControlbar"
-      @btnPlayClicked="btnPlayClicked"
-      @timeChanged="dragged"
-      @volumeChanged="volumeChanged"
-      @reqFullScreen="reqFullScreen"
-      @danmakuInputValueChanged="danmakuInputValueChanged"
-    />
-  </section>
 </template>
 
 <script lang="ts">
-  import {Component, Vue, Prop} from 'vue-property-decorator'
-  import {DanmakuMachine} from '@/lib/danmakuMachine'
-  import {BilibiliDanmaku, DanmakuType} from '@/lib/types/danmaku'
-  import Danmaku from "@/components/Danmaku.vue"
-  import VideoControlbar from "@/components/VideoControlbar.vue"
+import { Component, Vue, Prop } from "vue-property-decorator";
+import { DanmakuMachine } from "@/lib/danmakuMachine";
+import { BilibiliDanmaku, DanmakuType } from "@/lib/types/danmaku";
+import Danmaku from "@/components/Danmaku.vue";
+import VideoControlbar from "@/components/VideoControlbar.vue";
 
-  @Component({
-    name: 'Video-wrapper',
-    components: {VideoControlbar, Danmaku}
-  })
-  export default class VideoWrapper extends Vue{
-    danmakuMachine: DanmakuMachine|null = null
-    readonly threshold = 1.5
+@Component({
+    name: "Video-wrapper",
+    components: { VideoControlbar, Danmaku }
+})
+export default class VideoWrapper extends Vue {
+    danmakuMachine: DanmakuMachine | null = null;
+    readonly threshold = 1.5;
 
-    @Prop({type: String, default: '', required: true}) //
-    videoURL
+    @Prop({ type: String, default: "", required: true }) //
+    videoURL;
 
-    @Prop({type: String, default: '', required: true})
-    danmakuURL
-
+    @Prop({ type: String, default: "", required: true })
+    danmakuURL;
 
     topDanmakuPool = Array<BilibiliDanmaku>();
     bottomDanmakuPool = Array<BilibiliDanmaku>();
@@ -101,168 +89,197 @@
 
     loaded: number = 0;
 
-    isFullscreen = false
+    isFullscreen = false;
 
-    wrapperWidth = '100%'
-    controlbarWidth: string = 'auto';
-
+    wrapperWidth = "100%";
+    controlbarWidth: string = "auto";
 
     mounted() {
-      this.danmakuMachine = new DanmakuMachine(undefined, undefined, this.danmakuURL);
+        this.danmakuMachine = new DanmakuMachine(
+            undefined,
+            undefined,
+            this.danmakuURL
+        );
 
-      this.danmakuMachine.danmakuStream.subscribe((danmaku: BilibiliDanmaku) => {
-        const mode = danmaku.getDanmakuType()
-        if (mode !== DanmakuType.LTR && mode !== DanmakuType.RTL) {
-          // handle LTR and RTL separately temporarily
-          if (mode === DanmakuType.TOP) {
-            this.topDanmakuPool.push(danmaku)
-          } else if (mode === DanmakuType.BOTTOM) {
-            this.bottomDanmakuPool.push(danmaku)
-          }
-        } else if (mode === DanmakuType.RTL) {
-          const poolLen = this.normalDanmakuPool.length
-          if (poolLen === 0) {
-            this.normalDanmakuPool.push([danmaku])
-          } else {
-            const columnLength: number = this.normalDanmakuPool[poolLen - 1].length
-            const lastone: BilibiliDanmaku = this.normalDanmakuPool[poolLen - 1][columnLength - 1]
-            if (Math.abs(lastone.time - danmaku.time) < this.threshold) {
-              // console.log(this.normalDanmakuPool)
-              this.normalDanmakuPool[poolLen - 1].push(danmaku)
-            } else {
-              this.normalDanmakuPool.push([danmaku])
+        this.danmakuMachine.danmakuStream.subscribe(
+            (danmaku: BilibiliDanmaku) => {
+                const mode = danmaku.getDanmakuType();
+                if (mode !== DanmakuType.LTR && mode !== DanmakuType.RTL) {
+                    // handle LTR and RTL separately temporarily
+                    if (mode === DanmakuType.TOP) {
+                        this.topDanmakuPool.push(danmaku);
+                    } else if (mode === DanmakuType.BOTTOM) {
+                        this.bottomDanmakuPool.push(danmaku);
+                    }
+                } else if (mode === DanmakuType.RTL) {
+                    const poolLen = this.normalDanmakuPool.length;
+                    if (poolLen === 0) {
+                        this.normalDanmakuPool.push([danmaku]);
+                    } else {
+                        const columnLength: number = this.normalDanmakuPool[
+                            poolLen - 1
+                        ].length;
+                        const lastone: BilibiliDanmaku = this.normalDanmakuPool[
+                            poolLen - 1
+                        ][columnLength - 1];
+                        if (
+                            Math.abs(lastone.time - danmaku.time) <
+                            this.threshold
+                        ) {
+                            // console.log(this.normalDanmakuPool)
+                            this.normalDanmakuPool[poolLen - 1].push(danmaku);
+                        } else {
+                            this.normalDanmakuPool.push([danmaku]);
+                        }
+                    }
+                }
             }
-          }
-        }
-      });
+        );
 
-      (this.$refs.video as HTMLVideoElement).addEventListener('progress', (e: Event)=> {
-        const videoEl = e.target as HTMLVideoElement
-        this.loaded = videoEl.buffered.end(0)
-        console.log(this.loaded, this.totalTime)
-      })
+        (this.$refs.video as HTMLVideoElement).addEventListener(
+            "progress",
+            (e: Event) => {
+                const videoEl = e.target as HTMLVideoElement;
+                this.loaded = videoEl.buffered.end(0);
+                // console.log(this.loaded, this.totalTime);
+            }
+        );
+
+        document.addEventListener("fullscreenchange", ev => {
+            if (document.fullscreenElement) {
+                // The browser is in full-screen mode, with document.fullscreenElement
+                // being the element presented full-screen.
+                this.wrapperWidth = "100vw";
+            } else {
+                this.wrapperWidth = "100%";
+            }
+        });
     }
 
-    loadedmetadata(ev: Event){
-      const duration = (ev.target as HTMLVideoElement).duration
-      this.totalTime = duration
-      this.currentTime = 0
+    loadedmetadata(ev: Event) {
+        const duration = (ev.target as HTMLVideoElement).duration;
+        this.totalTime = duration;
+        this.currentTime = 0;
     }
 
-    durationChanged(ev: Event){
-      const duration = (ev.target as HTMLVideoElement).duration
-      this.totalTime = duration
-      this.currentTime = 0
+    durationChanged(ev: Event) {
+        const duration = (ev.target as HTMLVideoElement).duration;
+        this.totalTime = duration;
+        this.currentTime = 0;
     }
 
     timeChange(ev: Event) {
-      const time = (ev.target as HTMLVideoElement).currentTime
-      this.danmakuMachine.currentTime = time
-      this.currentTime = time
+        const time = (ev.target as HTMLVideoElement).currentTime;
+        this.danmakuMachine.currentTime = time;
+        this.currentTime = time;
     }
 
-    volumeChanged(ev: number){
-      (this.$refs.video as HTMLVideoElement).volume = ev
+    volumeChanged(ev: number) {
+        (this.$refs.video as HTMLVideoElement).volume = ev;
     }
 
     dragged(ev: number) {
-      (this.$refs.video as HTMLVideoElement).currentTime = ev
+        (this.$refs.video as HTMLVideoElement).currentTime = ev;
     }
 
-    btnPlayClicked(ev: boolean){
-      const video = this.$refs.video as HTMLVideoElement
-      if(ev){
-        video.play()
-        this.ongoing = true
-      }
-      else {
-        video.pause()
-        this.ongoing = false
-      }
+    btnPlayClicked(ev: boolean) {
+        const video = this.$refs.video as HTMLVideoElement;
+        if (ev) {
+            video.play();
+            this.ongoing = true;
+        } else {
+            video.pause();
+            this.ongoing = false;
+        }
     }
 
     reqFullScreen(ev: boolean) {
-      const ele =  (this.$refs.videoWrapper as HTMLElement)
-      if(!document.fullscreenElement){
-        ele.requestFullscreen().then(() => this.isFullscreen = true)
-        this.wrapperWidth = '100vw'
-      }
-      else{
-        document.exitFullscreen().then(() => this.isFullscreen = false)
-        this.wrapperWidth = '100%'
-      }
+        const ele = this.$refs.videoWrapper as HTMLElement;
+        if (!document.fullscreenElement) {
+            ele.requestFullscreen().then(() => (this.isFullscreen = true));
+        } else {
+            document.exitFullscreen().then(() => (this.isFullscreen = false));
+        }
     }
 
+    // onKeydown(ev: KeyboardEvent) {
+    //     if (ev.key === "Escape") {
+    //         ev.preventDefault();
+    //         document.exitFullscreen();
+    //         this.wrapperWidth = "100%";
+    //     }
+    // }
+
     onPlay() {
-      this.ongoing = true
+        this.ongoing = true;
     }
 
     onPause() {
-      this.ongoing = false
+        this.ongoing = false;
     }
 
     onWaiting() {
-      this.ongoing = false
+        this.ongoing = false;
     }
 
-    onDanmakuShouldRemove(data: {id: string, mode: DanmakuType}){
-      // console.log(this.topDanmakuPool, data.id, data.mode, DanmakuType.TOP)
-      if(data.mode !== DanmakuType.RTL && data.mode !== DanmakuType.LTR){
-        if(data.mode === DanmakuType.TOP){
-          const index = this.topDanmakuPool.findIndex(d => d.rowID === data.id)
-          this.topDanmakuPool.splice(index, 1)
+    onDanmakuShouldRemove(data: { id: string; mode: DanmakuType }) {
+        // console.log(this.topDanmakuPool, data.id, data.mode, DanmakuType.TOP)
+        if (data.mode !== DanmakuType.RTL && data.mode !== DanmakuType.LTR) {
+            if (data.mode === DanmakuType.TOP) {
+                const index = this.topDanmakuPool.findIndex(
+                    d => d.rowID === data.id
+                );
+                this.topDanmakuPool.splice(index, 1);
+            } else if (data.mode === DanmakuType.BOTTOM) {
+                const index = this.bottomDanmakuPool.findIndex(
+                    d => d.rowID === data.id
+                );
+                this.bottomDanmakuPool.splice(index, 1);
+            }
+        } else if (data.mode === DanmakuType.RTL) {
+            if (this.normalDanmakuPool[0].length > 1) {
+                const index = this.normalDanmakuPool[0].findIndex(
+                    d => d.rowID === data.id
+                );
+                this.normalDanmakuPool[0].splice(index, 1);
+            } else {
+                this.normalDanmakuPool.splice(0, 1);
+            }
         }
-        else if(data.mode === DanmakuType.BOTTOM) {
-          const index = this.bottomDanmakuPool.findIndex(d => d.rowID === data.id)
-          this.bottomDanmakuPool.splice(index, 1)
-        }
-      }
-      else if(data.mode === DanmakuType.RTL){
-        if(this.normalDanmakuPool[0].length > 1){
-          const index = this.normalDanmakuPool[0].findIndex(d => d.rowID === data.id)
-          this.normalDanmakuPool[0].splice(index, 1)
-        }
-        else {
-          this.normalDanmakuPool.splice(0, 1)
-        }
-      }
     }
 
-
-    danmakuValue = ''
+    danmakuValue = "";
     onTyping = false;
-    danmakuInputValueChanged(ev: string){
-      this.danmakuValue = ev
-      if(ev.length !== 0){
-        this.onTyping = true
-      } else {
-        this.onTyping = false
-      }
+    danmakuInputValueChanged(ev: string) {
+        this.danmakuValue = ev;
+        if (ev.length !== 0) {
+            this.onTyping = true;
+        } else {
+            this.onTyping = false;
+        }
     }
-
 
     showControlbar = false;
-    timeoutID: number|null = null
-    onMousemove(){
-      if(!this.timeoutID) {
-        this.showControlbar = true
-        this.timeoutID = setTimeout(() => {
-          if(!this.onTyping){
-            this.timeoutID = null;
-            this.showControlbar = false;
-          }
-          else {
-              this.timeoutID = null;
-              this.onMousemove();
-          }
-        }, 2000)
-      }
+    timeoutID: number | null = null;
+    onMousemove() {
+        if (!this.timeoutID) {
+            this.showControlbar = true;
+            this.timeoutID = setTimeout(() => {
+                if (!this.onTyping) {
+                    this.timeoutID = null;
+                    this.showControlbar = false;
+                } else {
+                    this.timeoutID = null;
+                    this.onMousemove();
+                }
+            }, 2000);
+        }
     }
-  }
+}
 </script>
 
 <style scoped lang="scss">
-  .video-wrapper {
+.video-wrapper {
     /*position: relative;*/
     display: grid;
     grid-template-rows: auto;
@@ -270,20 +287,21 @@
     justify-items: center;
     width: 75vw;
     height: 70vh;
-  }
+}
 
-  video {
+video {
     display: inline-block; /** prevent extra space of bottom */
     width: 100%;
     height: 100%;
     object-fit: fill;
-  }
+}
 
-  .danmaku-wrapper {
+.danmaku-wrapper {
     position: relative;
-  }
+    overflow: hidden;
+}
 
-  .danmaku-column {
+.danmaku-column {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -292,11 +310,10 @@
     width: 100%;
     height: 100%;
     overflow: hidden;
-  }
+}
 
-  .danmaku-column-reversed {
+.danmaku-column-reversed {
     @extend .danmaku-column;
     flex-direction: column-reverse;
-  }
-
+}
 </style>
